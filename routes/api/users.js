5c9e76b55eb61d9891c369ceb8d7aafd2b17b602
shrudder <seanmrudd@me.com>
2019-08-02 10:14:12 -0400

    added boilerplate to file structure

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const router = require('express').Router();
+const auth = require('../auth');
+const Users = mongoose.model('Users');
+
+router.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+module.exports = router;
